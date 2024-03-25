38f0e0d91fa466533a461ff198f3f31bb727d319
Kyle Scudder <kyle@kylescudder.co.uk>
2024-03-25 23:52:00 +0000

    feat(guests): guest data loading onto page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,23 @@
-import { SignIn, SignOutButton, useUser } from "@clerk/nextjs";
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import type Guest from "~/models/guest";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const user = useUser()
+  const user = useUser();
+  const data = api.guest.getAll.useQuery();
+  const guests: Guest[] = data.data ?? [];
+
   return (
     <div>
       Home
       <div>
-        {!user.isSignedIn && <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />}
+        {!user.isSignedIn && <SignInButton />}
         {!!user.isSignedIn && <SignOutButton />}
+      </div>
+      <div>
+        {guests.map((guest: Guest) => (
+          <div key={guest.id}>{guest.name}</div>
+        ))}
       </div>
     </div>
   );
