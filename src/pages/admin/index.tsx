d3281df2fbfb2a47b4fbada4db82656c703440dd
Kyle Scudder <kyle@kylescudder.co.uk>
2024-03-26 20:55:13 +0000

    feat(guest): moved get guest logic to admin pages

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,5 +1,20 @@
+import { type Guest } from "@prisma/client";
+import { api } from "~/utils/api";
+
 export default function Admin() {
+  const data = api.guest.getAll.useQuery();
+  const guests: Guest[] = data.data ?? [];
+
   return (
-    <div>Admin</div>
+    <main className="flex justify-center h-screen">
+      <div className="w-full h-full md:max-w-2xl border-x border-slate-400">
+        <div>Admin</div>
+        <div>
+          {guests.map((guest: Guest) => (
+            <div key={guest.id}>{guest.name}</div>
+            ))}
+        </div>
+      </div>
+    </main>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
-import type Guest from "~/models/guest";
-import { api } from "~/utils/api";
 
 export default function Home() {
   const user = useUser();
-  const data = api.guest.getAll.useQuery();
-  const guests: Guest[] = data.data ?? [];
 
   return (
     <div>
@@ -13,11 +9,6 @@ export default function Home() {
       <div>
         {!user.isSignedIn && <SignInButton />}
         {!!user.isSignedIn && <SignOutButton />}
-      </div>
-      <div>
-        {guests.map((guest: Guest) => (
-          <div key={guest.id}>{guest.name}</div>
-        ))}
       </div>
     </div>
   );
