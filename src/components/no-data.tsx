4141cb7807ee54424a24c33f72edd8f89692cd4c
Kyle Scudder <kyle@kylescudder.co.uk>
2024-03-27 21:44:20 +0000

    feat(no-data): added no data component

---
+++ src/components/no-data.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NoData = () => {
+  return (
+    <div>Something went wrong!</div>
+  )
+}
+
+export default NoData
--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,18 +1,23 @@
 import { type Guest } from "@prisma/client";
 import LoadingSpinner from "~/components/loading-spinner";
+import NoData from "~/components/no-data";
 import { api } from "~/utils/api";
 
 export default function Admin() {
   const { data, isLoading } = api.guest.getAll.useQuery();
+
+  if (isLoading) return <NoData />;
 
   if (!data) return <LoadingSpinner />;
 
   return (
     <div>
       <div>Admin</div>
-      <div>
-        {guests.map((guest: Guest) => (
-          <div key={guest.id}>{guest.name}</div>
+      <div className="flex flex-col">
+        {[...data, ...data].map((guest: Guest) => (
+          <div key={guest.id} className="border-b border-slate-400 p-8">
+            {guest.name}
+          </div>
           ))}
       </div>
     </div>
